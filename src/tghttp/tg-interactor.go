781db46ee22681d8bae48f9bfa42ede8bb5c7316
logotipiwe <german.reus@bk.ru>
2024-02-29 21:32:17 +0600

    Fix known group names

--- src/tghttp/tg-interactor.go
+++ src/tghttp/tg-interactor.go
@@ -278,7 +278,7 @@ func formatKnownChats(chats []*domain.KnownChat) string {
 		err := json.Unmarshal([]byte(chat.ChatInfo), &update)
 		if err == nil {
 			id := strconv.FormatInt(update.Message.Chat.ID, 10)
-			title := update.Message.Chat.UserName
+			title := pkg.GetChatNameFromUpdate(update)
 			ans += title + " (" + id + ")"
 			if update.Message.Chat.IsGroup() {
 				ans += " group"
